f23a22264d2ce66f3495db772e7258865cc8e5ad
Mangbung2 <41246591+Mangbung2@users.noreply.github.com>
2021-10-30 18:21:57 +0900

    POSTGRESQL적용

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['3.37.252.11']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'p-}6L_cFZud<6;f=|QDnmhD1h]%Shn>G',
+        'HOST': 'ls-2ae92affc0cdb022150f0c6dbde4fda896d79f52.cvr2ypsq2s8v.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
